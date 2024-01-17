558de1db8cac150fe361d0b67229bb283de2c53c
siu98 <101093309+siu98@users.noreply.github.com>
2024-01-17 12:59:33 +0900

    [Silver III] Title: 1로 만들기, Time: 576 ms, Memory: 38932 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,10 @@
+#백준 1463 : 1로 만들기
+N = int(input())
+dp = [0]*1000001
+for i in range(2, 1000001):
+    dp[i] = dp[i-1] + 1
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2]+1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3]+1)
+print(dp[N])
