780157485fa3378588c31ce77a0e08344b84fb7f
siu98 <101093309+siu98@users.noreply.github.com>
2024-02-14 23:26:22 +0900

    [Silver IV] Title: 30, Time: 76 ms, Memory: 32252 KB -BaekjoonHub

---
+++ 백준/Silver IV/10610. 30/30.py
@@ -0,0 +1,13 @@
+#백준10610 : 30
+N = input()
+N = sorted(N, reverse = True)
+sum = 0
+if '0' not in N:
+    print(-1)
+else:
+    for i in N:
+        sum += int(i)
+    if sum % 3 != 0:
+        print(-1)
+    else:
+        print(''.join(N))
