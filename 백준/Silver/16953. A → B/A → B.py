777da6b0a7e041717ff670c91a58f17bf21fe627
siu98 <101093309+siu98@users.noreply.github.com>
2024-05-14 11:11:36 +0900

    [Silver II] Title: A → B, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.py
@@ -0,0 +1,18 @@
+#백준16953: A->B
+import sys
+input = sys.stdin.readline
+A, B = map(int, input().split())
+num = 1
+while(B!=A):
+    num += 1
+    temp = B
+    
+    if B % 10 == 1:
+        B //= 10
+    elif B % 2 == 0:
+        B //= 2
+    if temp==B:
+        print(-1)
+        break
+else:
+    print(num)
