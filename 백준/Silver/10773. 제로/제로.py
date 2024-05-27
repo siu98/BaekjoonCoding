4270541f50c469dc617eb35ddf6c60cfd847d2e1
siu98 <101093309+siu98@users.noreply.github.com>
2024-05-27 18:12:59 +0900

    [Silver IV] Title: 제로, Time: 4000 ms, Memory: 31900 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,11 @@
+#백준10773: 제로
+K = int(input())
+stack = [] 
+
+for i in range(K): 
+    num = int(input())
+    if num == 0: 
+        stack.pop()
+    else:
+        stack.append(num) 
+print(sum(stack))
