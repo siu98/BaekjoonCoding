2f639cb06edf771e8dfe4518a2e23afaf581a0b3
siu98 <101093309+siu98@users.noreply.github.com>
2024-07-30 09:56:07 +0900

    [Silver V] Title: 숫자 카드, Time: 528 ms, Memory: 129700 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,18 +1,23 @@
-#백준10815: 숫자 카드
+#백준10815-1: 숫자카드
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-cards = list(map(int, input().split()))
-M = int(sys.stdin.readline())
-checks = list(map(int, input().split()))
+N = int(input().strip())        # 숫자 카드의 개수
+# for _ in range(N):
+card = set(map(int, input().split()))
 
-dict = {} 
-for i in range(len(cards)):
-    dict[cards[i]] = 0  
+res = []
+M = int(input().strip())       # 구해야할 정수의 개수
+# for _ in range(M):
+isCard = list(map(int, input().split()))
 
-for j in range(M):
-    if checks[j] not in dict:
-        print(0, end=' ')
+for num in isCard:
+    if num in card:
+        res.append(1)
     else:
-        print(1, end=' ')
+        res.append(0)
+
+# for a in res:
+#     print(a, end = " ")
+
+print(' '.join(map(str, res)))
