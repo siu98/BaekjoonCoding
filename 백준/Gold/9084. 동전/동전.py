c47cdc66364fa18e2fa956e9dfccaa90886996aa
siu98 <101093309+siu98@users.noreply.github.com>
2024-08-03 09:40:19 +0900

    [Gold V] Title: 동전, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.py
@@ -0,0 +1,19 @@
+#백준9084: 동전
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    coins = list(map(int, input().split()))
+    M = int(input())
+
+    d = [0] * (M + 1)
+    d[0] = 1
+
+    for coin in coins:
+        for i in range(M + 1):
+            if i >= coin:
+                d[i] += d[i - coin]
+
+    print(d[M])
