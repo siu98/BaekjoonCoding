f87cb89d3afff576fba30eb342b3d877ed13e77a
siu98 <101093309+siu98@users.noreply.github.com>
2025-02-12 14:21:06 +0900

    [Silver III] Title: 2×n 타일링, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,8 +1,13 @@
-# 백준 11726 : 2xn 타일링
+#백준11726 : 2xn 타일링 
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-tile = [0]*1001
-tile[1] = 1
-tile[2] = 2
-for i in range(3, 1001):
-    tile[i] = (tile[i-1] + tile[i-2]) % 10007
-print(tile[n]) 
+dp = [0] * 1000
+dp[0] = 1
+dp[1] = 2
+
+for i in range(2, n):
+    dp[i] = (dp[i-1] + dp[i-2]) % 10007
+
+print(dp[n-1])
